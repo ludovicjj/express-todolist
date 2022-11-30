1196cd81edd2c866f67c89e334e36ec5fa63121d
ludovicjj <jahanlud@gmail.com>
2022-11-30 13:16:19 +0100

    Add middleware serve-favicon

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const item_routes = require('./route/itemRoute');
-const session = require('cookie-session'); // session middleware
-const morgan = require('morgan')
+const session = require('cookie-session');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
 const app = express();
 const port = 8080
 
 app
-    // middleware cookies session
     .use(session({secret: 'appsecret'}))
-    // middleware log req info
+    .use(favicon(__dirname + "/public/favicon.ico"))
     .use(morgan('dev'))
-    // middleware define static dir (img/js/css)
     .use(express.static(__dirname + "/public"))
     // middleware router
     .use("/api", item_routes)
