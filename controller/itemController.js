7b30d4ba175c527bec9b44f0dec70c3ea0b4be8f
ludovicjj <jahanlud@gmail.com>
2022-12-01 13:43:22 +0100

    Handle create item and write new item into item_data.json

--- controller/itemController.js
+++ controller/itemController.js
@@ -1,5 +1,7 @@
-const {success} = require('../helper');
-const items = require('../mock-item');
+const fs = require('node:fs');
+const {success, error, getUniqueId} = require('../helper');
+const pathItemData = "./items_data.json";
+const items = JSON.parse(fs.readFileSync(pathItemData, "utf8"));
 
 exports.item_detail = (req, res) => {
     const id = parseInt(req.params.id);
@@ -9,4 +11,18 @@ exports.item_detail = (req, res) => {
 
 exports.item_list = (req, res) => {
     res.json(success('OK', items))
+}
+exports.item_add = (req, res) => {
+    const {name} = req.body
+    if (name !== undefined && name !== '') {
+        const id = getUniqueId(items);
+        let itemCreated = {id, name, created: new Date()}
+        items.push(itemCreated);
+        fs.writeFileSync(pathItemData, JSON.stringify(items, null, 4))
+        res.status(201);
+        res.json(success('Created', itemCreated, 201))
+    } else {
+        res.status(422);
+        res.json(error('Unprocessable entity', {message: "Missing field name"}, 422))
+    }
 }
