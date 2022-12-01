002d67d37b5b17f02d9f1b7d8e077f5b6c9e7981
ludovicjj <jahanlud@gmail.com>
2022-12-01 12:24:20 +0100

    Get unique id for created item

--- helper.js
+++ helper.js
@@ -1,3 +1,16 @@
 exports.success = (message, data, status = 200) => {
     return {message, status, data}
 }
+exports.error = (message, data, status = 400) => {
+    return {message, status, data}
+}
+
+exports.getUniqueId = (items) => {
+    if (!items.length) {
+        return 1;
+    }
+    const ids = items.map(item => item.id)
+    const id = ids.reduce((acc, id) => Math.max(acc, id), -Infinity)
+
+    return id + 1;
+}
