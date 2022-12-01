d5b46b86c14b1e6ac7706e68361a024b68704316
ludovicjj <jahanlud@gmail.com>
2022-12-01 12:26:32 +0100

    Add middleware to parse JSON bodies before the definition of endpoints

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const item_routes = require('./route/itemRoute');
-const session = require('cookie-session');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const app = express();
 const port = 8080
 
 app
-    .use(session({secret: 'appsecret'}))
     .use(favicon(__dirname + "/public/favicon.ico"))
     .use(morgan('dev'))
     .use(express.static(__dirname + "/public"))
+    .use(express.json())
     // middleware router
     .use("/api", item_routes)
     .listen(port, () => console.log(`Server run on : http://localhost:${port}`));
