aad82720e855775218895e9b244dd86d1f214e17
ludovicjj <jahanlud@gmail.com>
2022-12-01 12:27:18 +0100

    Add endpoint to add item (POST)

--- route/itemRoute.js
+++ route/itemRoute.js
@@ -7,5 +7,6 @@ const item_controller = require("../controller/itemController");
 router
     .get('/items/:id', item_controller.item_detail)
     .get('/items', item_controller.item_list)
+    .post('/items', item_controller.item_add)
 
 module.exports = router;
