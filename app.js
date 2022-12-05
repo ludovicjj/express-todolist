84aded69c7529bef5ace2037bcbf4d6ff7a42157
ludovicjj <jahanlud@gmail.com>
2022-12-05 12:31:36 +0100

    Try auth to mariadb database

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express');
 const item_routes = require('./route/itemRoute');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
+const { Sequelize } = require('sequelize');
+
 const app = express();
 const port = 8080;
+const sequelize = new Sequelize('mariadb://root:root@localhost:3306/todo_db', {
+    logging: false,
+    dialectOptions: {
+        timezone: '+01:00'
+    }
+});
+
+sequelize.authenticate()
+    .then(_ => {
+        console.log('Connection has been established successfully.');
+    }).catch(error => {
+        console.error('Unable to connect to the database:', error);
+    })
+
 
 app
     .use(favicon(__dirname + "/public/favicon.ico"))
