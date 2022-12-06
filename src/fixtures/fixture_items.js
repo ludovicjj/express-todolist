8da0962805270685a92436f320561cc2672dae9f
ludovicjj <jahanlud@gmail.com>
2022-12-06 12:34:52 +0100

    Rename category to categories

--- src/fixtures/fixture_items.js
+++ src/fixtures/fixture_items.js
@@ -1,9 +1,9 @@
 module.exports = [
-    {title: "title 1", content: "content item 1", published: true, category: ['php', 'Twig', 'Symfony']},
-    {title: "title 2", content: "content item 2", published: false, category: ['ES6']},
-    {title: "title 3", content: "content item 3", published: true, category: ['NodeJS']},
-    {title: "title 4", content: "content item 4", published: true, category: ['Sequelize', 'Fixtures']},
-    {title: "title 5", content: "content item 5", published: true, category: ['Design pattern']},
-    {title: "title 6", content: "content item 6", published: true, category: ['Router']},
-    {title: "title 7", content: "content item 7", published: false, category: ['SQL', 'Mariadb']},
+    {title: "title 1", content: "content item 1", published: true, categories: ['php', 'Twig', 'Symfony']},
+    {title: "title 2", content: "content item 2", published: false, categories: ['ES6']},
+    {title: "title 3", content: "content item 3", published: true, categories: ['NodeJS']},
+    {title: "title 4", content: "content item 4", published: true, categories: ['Sequelize', 'Fixtures']},
+    {title: "title 5", content: "content item 5", published: true, categories: ['Design pattern']},
+    {title: "title 6", content: "content item 6", published: true, categories: ['Router']},
+    {title: "title 7", content: "content item 7", published: false, categories: ['SQL', 'Mariadb']},
 ];
