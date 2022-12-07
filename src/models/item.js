1409cc6cdc4e519dbc8b8e057f8f2333e70c68db
ludovicjj <jahanlud@gmail.com>
2022-12-07 17:56:16 +0100

    Item Model: add constraints

--- src/models/item.js
+++ src/models/item.js
@@ -7,15 +7,35 @@ module.exports = (sequelize, DataTypes) => {
         },
         title: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: { msg: "Property title is required." },
+                notEmpty: { args: true, msg: "Property title cannot be empty."}
+            }
         },
         content: {
             type: DataTypes.TEXT,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: { msg: "Property content is required." },
+                notEmpty: { args: true, msg: "Property content cannot be empty."}
+            }
         },
         categories: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                customValidator(value) {
+                    if (value.split(',').includes('bad category')) {
+                        throw new Error("invalid category.");
+                    }
+                },
+                maxCategoriesValidator(value) {
+                    if (value.split(',').length > 3) {
+                        throw new Error("An item cannot have more than 3 categories.");
+                    }
+                }
+            },
             get() {
                 return this.getDataValue('categories').split(',');
             },
@@ -26,7 +46,14 @@ module.exports = (sequelize, DataTypes) => {
         published: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
-            defaultValue: false
+            defaultValue: false,
+            validate: {
+                isBoolean(value) {
+                    if (value !== true && value !== false && toString.call(value) !== '[object Boolean]') {
+                        throw new Error("Property published expected boolean.");
+                    }
+                }
+            }
         }
     }, {
         timestamps: true
