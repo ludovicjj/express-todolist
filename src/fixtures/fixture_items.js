5ec50988315995f50001ac0459f10023c634bda6
ludovicjj <jahanlud@gmail.com>
2022-12-08 17:59:41 +0100

    Update titles into item fixture

--- src/fixtures/fixture_items.js
+++ src/fixtures/fixture_items.js
@@ -1,9 +1,9 @@
 module.exports = [
-    {title: "title 1", content: "content item 1", published: true, categories: ['php', 'Twig', 'Symfony']},
-    {title: "title 2", content: "content item 2", published: false, categories: ['ES6']},
-    {title: "title 3", content: "content item 3", published: true, categories: ['NodeJS']},
-    {title: "title 4", content: "content item 4", published: true, categories: ['Sequelize', 'Fixtures']},
-    {title: "title 5", content: "content item 5", published: true, categories: ['Design pattern']},
-    {title: "title 6", content: "content item 6", published: true, categories: ['Router']},
-    {title: "title 7", content: "content item 7", published: false, categories: ['SQL', 'Mariadb']},
+    {title: "development web", content: "content item 1", published: true, categories: ['php', 'Twig', 'Symfony']},
+    {title: "frontend js", content: "content item 2", published: false, categories: ['ES6']},
+    {title: "backend js", content: "content item 3", published: true, categories: ['NodeJS']},
+    {title: "orm", content: "content item 4", published: true, categories: ['Sequelize', 'Fixtures']},
+    {title: "organisation general", content: "content item 5", published: true, categories: ['Design pattern']},
+    {title: "organisation url", content: "content item 6", published: true, categories: ['Router']},
+    {title: "base de donnée", content: "content item 7", published: false, categories: ['SQL', 'Mariadb']},
 ];
