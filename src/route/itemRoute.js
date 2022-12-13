26d46a25110927b32881bf986258e0048af825a3
ludovicjj <jahanlud@gmail.com>
2022-12-13 16:07:48 +0100

    Add middleware auth for each item route

--- src/route/itemRoute.js
+++ src/route/itemRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../security/auth")
 
 // Require controller modules.
 const item_controller = require("../controller/itemController");
 
 router
-    .get('/items', item_controller.item_list)
-    .get('/items/:id', item_controller.item_detail)
-    .post('/items', item_controller.item_add)
-    .put('/items/:id', item_controller.item_update)
-    .delete('/items/:id', item_controller.item_delete)
+    .get('/items', auth, item_controller.item_list)
+    .get('/items/:id', auth, item_controller.item_detail)
+    .post('/items', auth, item_controller.item_add)
+    .put('/items/:id', auth, item_controller.item_update)
+    .delete('/items/:id', auth, item_controller.item_delete)
 
 module.exports = router;
