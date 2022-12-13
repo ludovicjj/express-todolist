d031fce919c8b708aedcd1022ff104b88a533a1e
ludovicjj <jahanlud@gmail.com>
2022-12-13 15:26:38 +0100

    Generate JWT token

--- src/controller/securityController.js
+++ src/controller/securityController.js
@@ -1,5 +1,10 @@
 const { User } = require("../database/sequelize");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const fs = require("fs")
+const jwt = require("jsonwebtoken");
+const secret = require("../../config/jwt/secret")
+const privateKey = fs.readFileSync("./config/jwt/private.pem");
+
 
 exports.user_login = (req, res) => {
     User.findOne({ where: { username: req.body.username } }).then(user => {
@@ -12,7 +17,14 @@ exports.user_login = (req, res) => {
                 return res.status(401).json({message: "Unauthorized", status: 401, errors: "Invalid username or password"})
             }
 
-            return res.json({message: "OK", status: 200, data: user})
+            // JWT
+            const token = jwt.sign(
+                { userId: user.id },
+                {key: privateKey, passphrase: secret},
+                { expiresIn: "1h", algorithm: "RS256" }
+            )
+
+            return res.json({message: "OK", status: 200, token})
         })
     }).catch(error => {
         return res.status(500).json({message: "Internal Server Error", status: 500, data: error})
