98884997e40f388600247856359bf8e8e4551a61
ludovicjj <jahanlud@gmail.com>
2022-12-13 12:34:47 +0100

    Update endpoint: /api/login

--- src/controller/securityController.js
+++ src/controller/securityController.js
@@ -2,15 +2,19 @@ const { User } = require("../database/sequelize");
 const bcrypt = require("bcrypt")
 
 exports.user_login = (req, res) => {
-    User.findOne({
-        where: { username: req.body.username}
-    }).then(user => {
+    User.findOne({ where: { username: req.body.username } }).then(user => {
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized", status: 401, errors: "Invalid username or password" })
+        }
+
         bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
-            if (isPasswordValid) {
-                return res.json({message: "OK", status: 200, data: user})
-            } else {
-                return res.json({message: "Not Found", status: 404, errors: "Invalid username or password"})
+            if (!isPasswordValid) {
+                return res.status(401).json({message: "Unauthorized", status: 401, errors: "Invalid username or password"})
             }
+
+            return res.json({message: "OK", status: 200, data: user})
         })
+    }).catch(error => {
+        return res.status(500).json({message: "Internal Server Error", status: 500, data: error})
     })
 }
