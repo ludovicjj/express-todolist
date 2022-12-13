ec1ca279a5f6f7750c7b13e22961dc1b9b42bfa6
ludovicjj <jahanlud@gmail.com>
2022-12-13 15:25:27 +0100

    Middleware: verify JWT token

---
+++ src/security/auth.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const publicKey = fs.readFileSync("./config/jwt/public.pem");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const authorization = req.headers.authorization
+
+    if (!authorization) {
+        res.status(401).json({message: "Unauthorized", status: 401, errors: "Missing JWT token in request header"})
+    }
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, publicKey, { algorithms: ['RS256'] },(error, decodedToken) => {
+        if (error) {
+            return res.status(401).json({message: "Unauthorized", status: 401, errors: error})
+        }
+        const userId = decodedToken.userId
+
+        if (req.body.userId && req.body.userId !== userId) {
+            return res.status(401).json({message: "Unauthorized", status: 401, errors: "a test"})
+        } else {
+            next();
+        }
+    })
+}
